ee546e6d6e25975f1faf0d5173879ebea71ff729
ckorir <chrisruto.k@gmail.com>
2023-09-25 22:34:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(drivers, name){
+    const matchingDriver = drivers.filter(driver =>
+        driver.toLowerCase() == name.toLowerCase()); // Changes both the driver and name to lower case for case sensitivity
+    return matchingDriver;
+}
+
+function fuzzyMatch(drivers, name){
+    const beginningLetters = drivers.filter(driver =>
+        driver.charAt(0) === name.charAt(0));
+    return beginningLetters;
+}
+
+function matchName(drivers, name){
+
+    const matchData = drivers.filter(data => data.name === name)
+    return matchData;
+
+}
